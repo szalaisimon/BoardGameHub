228ddde3d4e23c677f515903345020cf24b2d950
Szalai Simon <sszalai@combit.hu>
2024-07-25 19:12:24 +0200

    CORS beállítások

--- src/main/java/com/example/BoardGameHub/BoardGameHubApiApplication.java
+++ src/main/java/com/example/BoardGameHub/BoardGameHubApiApplication.java
@@ -2,10 +2,32 @@ package com.example.BoardGameHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BoardGameHubApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(BoardGameHubApiApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin",
+            "Content-Type", "Accept", "Authorization", "X-Requested-With", "Access-Control-Request-Method",
+            "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Accept", "Content-Type", "Authorization",
+            "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "DELETE", "POST", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
     }
 }
